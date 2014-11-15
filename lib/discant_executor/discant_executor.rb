7092a2198c59308fddc25514ee0fa1d1a380ea08
simon0191 <simon0191@gmail.com>
2014-11-15 15:40:56 -0500

    Add DiscantExecutor lib

---
+++ lib/discant_executor/discant_executor.rb
@@ -0,0 +1,29 @@
+require 'open3'
+class DiscantExecutor
+  @discantJarPath = "#{Rails.root}/lib/discant_executor/Discant_linux.jar"
+  class << self
+    attr_accessor :discantJarPath
+  end
+
+  def self.exec(filePath)
+    #TODO: add timeout
+    #check this gist: https://gist.github.com/lpar/1032297
+    #http://blog.bigbinary.com/2012/10/18/backtick-system-exec-in-ruby.html
+    cmd = "java -jar #{discantJarPath} #{filePath}"
+    out, err, exit_status = nil, nil, nil
+
+    Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
+      out = stdout.read
+      err = stderr.read
+      exit_status = wait_thr.value
+      puts "stdout is: " + out
+      puts "stderr is: " + err
+      puts "exit status: " + exit_status.to_s
+    end
+    return {
+      stdout: out,
+      stderr: err,
+      exit_status: exit_status
+    }
+  end
+end
