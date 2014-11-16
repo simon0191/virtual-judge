5e4a25ef99bd172dda2263152d5c2ec534b3902f
simon0191 <simon0191@gmail.com>
2014-11-16 14:18:08 -0500

    Add admin verification helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def verify_user_is_admin
+    unless signed_in? && current_user.role == 'admin'
+      begin
+        flash.alert = "Permisos insuficientes"
+        #TODO: Redirect to last pagenot working :(
+        redirect_to :back
+      rescue ActionController::RedirectBackError
+        redirect_to root_path
+      end
+    end
+  end
+
 end
